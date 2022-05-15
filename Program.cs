dd58d4798274170cc39cb5ec082f4a9a980cf437
xtnctx <ryanbahillo.10@gmail.com>
2022-05-15 20:56:39 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+/* Exercise from https://www.exercisescsharp.com/arithmetic/calculate-values-function */
+
+namespace ValuesFunction
+{
+
+    public class QuadraticFunction
+    {
+        static void Main()
+        {
+            Console.Write("x =  ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int xcopy = x; // used when original x = 0 because of the decrementation
+
+            Console.Write("abs(range) = ");
+            int range = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            List<int> output = new List<int>();
+
+
+            if (x < 0) // negative x
+            {
+
+                while (x <= range)
+                {
+                    output.Add(Curve(x));
+                    x++;
+                }
+            }
+            else // positive x
+            {
+                while (x >= 0)
+                {
+                    output.Add(Curve(x));
+                    x--;
+                }
+
+                if (range > xcopy)
+                {
+                    for (int i = 0; i < xcopy; i++) output.Add(Curve(i));
+                }
+
+                else
+                {
+                    for (int i = x; i < range; i++) output.Add(Curve(i));
+                }
+
+                
+            }
+
+            Console.WriteLine(string.Join(", ", output.ToArray()));
+            
+        }
+
+        public static int Curve(int x)
+        {
+            return (int) Math.Pow(x, 2) - 2*x + 1; // y = x^2 - 2x + 1
+
+        }
+
+
+  
+
+
+
+
+    }
+
+
+}
